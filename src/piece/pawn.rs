c180b26841b290ebc2053389842985900a1197d5
russellgoldman <russellgoldman1@gmail.com>
2024-07-13 16:10:03 -0400

    Documentation update

--- src/piece/pawn.rs
+++ src/piece/pawn.rs
@@ -11,6 +11,18 @@ pub struct Pawn {
 
 impl ChessPieceTrait for Pawn {
     fn valid_moves(&self) -> Option<Vec<BoardPosition>> {
+        /*
+            Look at the current position of the piece
+
+            - Pawn's can move one or two spaces forward only if they haven't been moved in the game yet
+            - Otherwise, Pawns' can only move one space forward
+            - Pawn's can capture enemy pieces by moving one space forward diagonally
+            - White Pawn's can only move forward in the range of rows 1 - 7 (increasing)
+            - Black Pawn's can only move forward in the range of rows 6 - 0 (decreasing)
+
+            For simplicity, we will not be implementing the En Passant rule.
+        */
+
         None // temporary value
     }
 }
